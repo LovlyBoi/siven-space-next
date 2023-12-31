41e55f049bcd8eac5482a1988af885a1e8f1bbfb
LovlyBoi <728881636@qq.com>
2023-12-31 18:20:14 +0800

    fix: 修复了layouts目录下无法应用tailwindcss的问题

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from 'react'
 export default function RootLayout({ children }: { children?: ReactNode }) {
   return (
     <div className="width-limit max-w-screen-lg mx-1 sm:mx-4 lg:mx-auto">
-      <header>Header</header>
+      <header className='text-red-300'>Header</header>
       <main>{children}</main>
     </div>
   )
