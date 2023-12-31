8c478966835e6af1e034e9633cb6f1405e99cd62
LovlyBoi <728881636@qq.com>
2024-01-01 00:49:07 +0800

    chore: 添加了环境变量

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,24 @@
-import type { FC, ReactNode } from 'react'
+import type { FC } from 'react'
 import RootLayout from '@/layouts/RootLayout'
 import CardContainer from '@/components/CardContainer'
 
-const PageIndex: FC = () => {
+const PageIndex: FC<{
+  axiosBaseUrl: string
+}> = ({ axiosBaseUrl }) => {
   return (
     <RootLayout>
+      <div>{axiosBaseUrl}</div>
       <CardContainer></CardContainer>
     </RootLayout>
   )
 }
 
 export default PageIndex
+
+export async function getStaticProps() {
+  return {
+    props: {
+      axiosBaseUrl: process.env.AXIOS_BASEURL,
+    },
+  }
+}
