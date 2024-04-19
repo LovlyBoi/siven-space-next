67fbb97f91f3e46907d76cc61e03ca411f955744
LovlyBoi <728881636@qq.com>
2024-04-19 09:55:01 +0800

    feat(layout.tsx): 添加了open graph头

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,29 @@ import DarkMode from './DarkMode'
 import './global.css'
 
 const RootLayout: FC<{ children?: ReactNode }> = ({ children }) => {
+  const openGraph = [
+    {
+      property: 'og:title',
+      content: '智文的前端小站',
+    },
+    {
+      property: 'og:description',
+      content: '一个分享前端技术、学习前端知识、记录前端生活的地方。',
+    },
+    {
+      property: 'og:image',
+      content: 'https://www.siven.cc/api/image/evOaviVv_hAu7QZBfMoTQ.webp',
+    },
+    {
+      property: 'og:author',
+      content: 'Siven',
+    },
+    {
+      property: 'og:url',
+      content: 'https://www.siven.cc',
+    },
+  ]
+
   return (
     <html lang="zh-CN">
       <link rel="icon" href="/favicon.ico" sizes="any" />
@@ -14,6 +37,9 @@ const RootLayout: FC<{ children?: ReactNode }> = ({ children }) => {
         name="keywords"
         content="node,vue,typescript,ts,react,前端,博客,技术,学习,生活,前端博客,前端技术博客,前端学习博客,前端生活博客"
       />
+      {openGraph.map((og) => (
+        <meta key={og.property} name={og.property} content={og.content} />
+      ))}
       <body>
         <DarkMode>{children}</DarkMode>
       </body>
