2f9d476f92a450f79ba79a05008fe1394ff40038
LovlyBoi <728881636@qq.com>
2024-06-13 09:13:51 +0800

    feat(article/page.tsx): 取消ssr中fetch请求的缓存

--- app/article/[id]/error.tsx
+++ app/article/[id]/error.tsx
@@ -17,7 +17,7 @@ export default function Error({
   }, [error])
 
   return (
-    <div className="font-sans w-screen h-screen flex justify-center items-center w-screen h-screen flex justify-center items-center">
+    <div className="font-sans w-screen h-screen flex justify-center items-center">
       <div className="h-[50px] flex items-center">
         <h1 className="h-full leading-[50px] text-[24px] pr-[24px] font-medium border-r border-gray-400">
           500
